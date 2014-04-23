e611015b9708c5b75edef910a00be523b24af0d1
Eric Tang <eric.x.tang@gmail.com>
2014-04-23 17:15:44 -0400

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,52 +1,30 @@
 #
-# Cookbook Name:: chrome
+# Cookbook Name:: chromium
 # Recipe:: default
 #
-# Copyright (c) 2013 Rally Software Development Corp
+# Copyright 2011, Justin Huff
 #
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
+ 
+include_recipe "apt"
 
-if node['platform_family'] == 'rhel'
-  yum_key 'google-chrome-key.pub' do
-    url 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
+apt_repository "chrome" do
+    uri "http://dl.google.com/linux/chrome/deb/"
+    distribution "stable"
+    components ["main"]
+    key "https://dl-ssl.google.com/linux/linux_signing_key.pub"
     action :add
-  end
-
-  yum_repository 'google-chrome' do
-    name 'google-chrome'
-    description 'Google Chrome'
-    url 'http://dl.google.com/linux/chrome/rpm/stable/x86_64'
-    key 'google-chrome-key.pub'
-    action :add
-  end
-elsif node['platform_family'] == 'debian'
-  include_recipe 'apt'
-
-  apt_repository 'google-chrome' do
-      uri 'http://dl.google.com/linux/chrome/deb/'
-      distribution 'stable'
-      components ['main']
-      key 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
-      action :add
-  end
 end
 
-package "google-chrome-#{node['chrome']['version']}"
+package "google-chrome-" + node['google-chrome']['track']
